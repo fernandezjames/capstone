ccd1f67d97dbfd9219aa16c1edfd04bce01ef67c
james <jamesfernandez473@gmail.com>
2017-12-07 18:51:21 +0800

    history

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -46,11 +46,7 @@
          </div>
          <div id="content">
             <div class="line">
-               <p class="s-12 m-12 l-8 center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil.
-               </p>
-               <h2>Some subtitle</h2>
-               <p class="s-12 m-12 l-8 center">Imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.
-               </p>
+               <p class="s-12 m-12 l-8 center" style="font-size:25px;">Maharlika Suite was established on June 5, 2007 at Maharlika Road, Brgy. Caanawan, San Jose City, Nueva Ecija. Ms. Melissa T. Sanqui is the owner of this company. Maharlika Suite has 21 single room, 2 family room, and 2 family suite room. Maharlika Suite has also other services such as swimming pool, grill and restobar. The company have been marketable in San Jose City and also beyond its vicinity for a past 1 decade until now.</p>
             </div>
          </div>
          
@@ -59,18 +55,15 @@
                <div id="owl-demo2" class="owl-carousel owl-theme">
                   <div class="item">
                      <h2>Amazing responsive template</h2>
-                     <p class="s-12 m-12 l-8 center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-                     </p>
+                     <p class="s-12 m-12 l-8 center"></p>
                   </div>
                   <div class="item">
                      <h2>Responsive components</h2>
-                     <p class="s-12 m-12 l-8 center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-                     </p>
+                     <p class="s-12 m-12 l-8 center"></p>
                   </div>
                   <div class="item">
                      <h2>Retina ready</h2>
-                     <p class="s-12 m-12 l-8 center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-                     </p>
+                     <p class="s-12 m-12 l-8 center"></p>
                   </div>
                </div>
             </div>
