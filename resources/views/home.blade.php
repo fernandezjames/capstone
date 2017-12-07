928747da2832c288431542b04810acbff2220638
HanamichiXave <johnxavierbuenaventura@gmail.com>
2017-12-07 22:47:21 +0800

    new

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,52 +43,19 @@
             <div id="owl-demo" class="owl-carousel owl-theme">
                <div class="item">
                   <img src="img/front.jpg" alt="">
-                  <div class="carousel-text">
-                     <div class="line">
-                        <div class="s-12 l-9">
-                           <h2>Theme based on Responsee framework</h2>
-                        </div>
-                        <div class="s-12 l-9">
-                           <p>With amazing responsive carousel
-                           </p>
-                        </div>
-                     </div>
-                  </div>
                </div>
                <div class="item">
                   <img src="img/room.jpg" alt="">
-                  <div class="carousel-text">
-                     <div class="line">
-                        <div class="s-12 l-9">
-                           <h2>Build new layout in 10 minutes!</h2>
-                        </div>
-                        <div class="s-12 l-9">
-                           <p>Lightweight, more intuitive and useful responsive CSS framework
-                           </p>
-                        </div>
-                     </div>
-                  </div>
                </div>
                <div class="item">
                   <img src="img/room1.jpg" alt="">
-                  <div class="carousel-text">
-                     <div class="line">
-                        <div class="s-12 l-9">
-                           <h2>Design theme is under the MIT license</h2>
-                        </div>
-                        <div class="s-12 l-9">
-                           <p>Best theme based on Responsee framework
-                           </p>
-                        </div>
-                     </div>
-                  </div>
                </div>
             </div>
          </div>
          <!-- FIRST BLOCK --> 	
          <div id="first-block">
             <div class="line">
-               <h2>OTHER SERVICES</h2><p>
+               <h2>ATTRACTIONS</h2><p>
                </p>
                <div class="margin">
                   <div class="s-12 m-6 l-3 margin-bottom">
