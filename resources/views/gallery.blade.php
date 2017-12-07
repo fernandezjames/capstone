1d9a2fb0f772f765fd4bee799a462d2a34df7149
AvyBy <avegin.sanchez@simplevia.com>
2017-12-07 16:15:36 +0800

    include navigation
    include navigation

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -22,61 +22,7 @@
    </head>
    <body class="size-1140">
       <!-- TOP NAV WITH LOGO -->  
-      <header>
-      <nav>
-            <div class="line">
-               <div class="top-nav">              
-                  <div class="logo hide-l">
-                     <img src="img/logo.png">
-                  </div>                  
-                  <p class="nav-text">Custom menu text</p>
-                  <div class="top-nav s-12 l-5">
-                     <ul class="right top-ul chevron">
-                        <li><a href="{{URL::Route('home')}}">Home</a>
-                        </li>
-                        <li><a href="{{URL::Route('product')}}">Product</a>
-                        </li>
-                        <li><a href="{{URL::Route('services')}}">Services</a>
-                        </li>
-                     </ul>
-                  </div>
-                  <ul class="s-12 l-2">
-                     <li class="logo hide-s hide-m">
-                        <img src="img/logo.png">
-                     </li>
-                  </ul>
-                  <div class="top-nav s-12 l-5">
-                     <ul class="top-ul chevron">
-                        <li><a href="{{URL::Route('gallery')}}">Gallery</a>
-                        </li>
-                        <li>
-                           <a>Company</a>         
-                           <ul>
-                              <li><a>Company 1</a>
-                              </li>
-                              <li><a>Company 2</a>
-                              </li>
-                              <li>
-                                 <a>Company 3</a>         
-                                 <ul>
-                                    <li><a>Company 3-1</a>
-                                    </li>
-                                    <li><a>Company 3-2</a>
-                                    </li>
-                                    <li><a>Company 3-3</a>
-                                    </li>
-                                 </ul>
-                              </li>
-                           </ul>
-                        </li>
-                        <li><a data-toggle="modal" data-target="#myModal">Login</a>
-                        </li>
-                     </ul> 
-                  </div>
-               </div>
-            </div>
-         </nav>
-      </header>
+      @include('nav')
       <section>
          <div id="head">
             <div class="line">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,51 +36,7 @@
    </head>
    <body class="size-1140">
       <!-- TOP NAV WITH LOGO -->  
-      <header>
-         <nav>
-            <div class="line">
-               <div class="top-nav">              
-                  <div class="logo hide-l">
-                     <img src="img/logo.png">
-                  </div>                  
-                  <p class="nav-text">Custom menu text</p>
-                  <div class="top-nav s-12 l-5">
-                     <ul class="right top-ul chevron">
-                        <li><a href="{{URL::Route('home')}}">Home</a>
-                        </li>
-                        <li>
-                           <a>Rooms</a>           
-                           <ul>
-                              <li><a>Single Room</a>
-                              </li>
-                              <li><a>Family Room</a>
-                              </li>
-                              <li><a>Family Suite</a>             
-                              </li>
-                           </ul>
-                        </li>
-                        <li><a href="{{URL::Route('product')}}">About</a>
-                        </li>
-                       
-                     </ul>
-                  </div>
-                  <ul class="s-12 l-2">
-                     <li class="logo hide-s hide-m">
-                        <img src="img/logo.png">
-                     </li>
-                  </ul>
-                  <div class="top-nav s-12 l-5">
-                     <ul class="top-ul chevron">
-                        
-                        
-                        <li><a class="pull-right" data-toggle="modal" data-target="#myModal">Login</a>
-                        </li>
-                     </ul> 
-                  </div>
-               </div>
-            </div>
-         </nav>
-      </header>
+      @include('nav')
       <section>
                  <!-- Modal -->
         <div class="modal fade" id="myModal" role="dialog">
